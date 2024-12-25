b6ffd2cd3c840db01a9d528bc82a680758e4bf1f
tim <timdesmet00@gmail.com>
2024-12-25 18:05:09 +0100

    first init

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const connectdb = require("./config/db");
+const dotenv = require("dotenv").config();
+
+const port = 5000;
+
+connectdb();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// ajout des routes pour les fonction CRUD
+
+// app.use("/comm", require("./router/comm.router"))
+
+app.listen(port, () => {
+  console.log(`Le server est démarré sur le port: ${port}`);
+});
