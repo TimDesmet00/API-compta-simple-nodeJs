65399c94bfee4888fe753bbddbf0fcc6af8c420a
tim <timdesmet00@gmail.com>
2024-12-26 09:25:21 +0100

    model facture mise en place

--- Models/facture.js
+++ Models/facture.js
@@ -1,5 +1,54 @@
 const mongoose = require("mongoose");
 
-const FactureSchema = new mongoose.Schema({});
+const FactureSchema = new mongoose.Schema({
+  numero: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  client: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "client",
+  },
+  moi: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "moi",
+  },
+  totalHT: {
+    type: Number,
+    required: true,
+  },
+  totalTVA: {
+    type: Number,
+    required: true,
+  },
+  totalTTC: {
+    type: Number,
+    required: true,
+  },
+  lignes: [
+    {
+      description: {
+        type: String,
+        required: true,
+      },
+      quantite: {
+        type: Number,
+        required: true,
+      },
+      pu: {
+        type: Number,
+        required: true,
+      },
+      tva: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+});
 
 module.exports = mongoose.model("facture", FactureSchema);
