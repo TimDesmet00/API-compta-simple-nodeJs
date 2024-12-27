a1236149a64fec17e65a9dd82dbb6cb11e87cbe9
tim <timdesmet00@gmail.com>
2024-12-27 10:10:11 +0100

    ajout des module validator

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ const connectdb = require("./config/db");
 const dotenv = require("dotenv").config();
 
 const port = 5000;
-
 connectdb();
-
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// ajout des routes pour les fonction CRUD
-
+// ajout des routes
 app.use("/client", require("./Router/client.router"));
 
 app.listen(port, () => {
