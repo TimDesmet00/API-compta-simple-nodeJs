db2cb4fdac02734e32bf4cf4c1383fb90773e3c2
tim <timdesmet00@gmail.com>
2024-12-29 08:56:46 +0100

    debug facture router

--- Router/facture.router.js
+++ Router/facture.router.js
@@ -6,7 +6,7 @@ router.post("/add", factureController.createFacture);
 router.get("/getall", factureController.getAllFactures);
 router.get("/getone/:id", factureController.getFactureById);
 router.get("/getclient/:id", factureController.getFactureByClient);
-router.get("/gatuser/:id", factureController.getFactureByUser);
+router.get("/getuser/:id", factureController.getFactureByUser);
 router.patch("/update/:id", factureController.updateFacture);
 router.delete("/delete/:id", factureController.deleteFacture);
 
