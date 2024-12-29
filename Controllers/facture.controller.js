15ad58b42bd5e056ed289a49facb2b80c190f01b
tim <timdesmet00@gmail.com>
2024-12-29 08:38:26 +0100

    facture controller fin des base

--- Controllers/facture.controller.js
+++ Controllers/facture.controller.js
@@ -74,6 +74,62 @@ const getFactureById = async (req, res) => {
   }
 };
 
+const getFactureByClient = async (req, res) => {
+  try {
+    const existingClient = await client.findById(req.params.id);
+    if (!existingClient) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Client not found",
+      });
+    }
+    const factures = await facture
+      .find({ client: req.params.id })
+      .populate("client")
+      .populate("user");
+    res.status(200).json({
+      status: "success",
+      data: {
+        factures,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message:
+        error.message || "An error occurred while retrieving the factures.",
+    });
+  }
+};
+
+const getFactureByUser = async (req, res) => {
+  try {
+    const existingUser = await user.findById(req.params.id);
+    if (!existingUser) {
+      return res.status(404).json({
+        status: "fail",
+        message: "User not found",
+      });
+    }
+    const factures = await facture
+      .find({ user: req.params.id })
+      .populate("client")
+      .populate("user");
+    res.status(200).json({
+      status: "success",
+      data: {
+        factures,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message:
+        error.message || "An error occurred while retrieving the factures.",
+    });
+  }
+};
+
 const updateFacture = async (req, res) => {
   if (!req.body) {
     return res.status(400).send({
@@ -110,4 +166,39 @@ const updateFacture = async (req, res) => {
   }
 };
 
-module.exports = { createFacture };
+const deleteFacture = async (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to delete can not be empty!",
+    });
+  }
+  const existingFacture = await facture.findById(req.params.id);
+  if (!existingFacture) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Facture not found",
+    });
+  }
+  try {
+    await facture.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: error.message || "An error occurred while deleting the facture.",
+    });
+  }
+};
+
+module.exports = {
+  createFacture,
+  getAllFactures,
+  getFactureById,
+  getFactureByClient,
+  getFactureByUser,
+  updateFacture,
+  deleteFacture,
+};
