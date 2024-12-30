5099f6c4f7cffa31f837ce965eb32fa771bee142
tim <timdesmet00@gmail.com>
2024-12-30 08:33:52 +0100

    la ca devrais le faire

--- Models/processInvoiceIn.model.js
+++ Models/processInvoiceIn.model.js
@@ -11,67 +11,67 @@ const ProcessInvoiceInSchema = new mongoose.Schema({
       message: (props) =>
         `${props.value} is not a valid period format! Use MM-YYYY.`,
     },
-    data: [
-      {
-        number: {
-          type: Number,
-          required: true,
-        },
-        date: {
-          type: Date,
-          required: true,
-        },
-        supplier: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "supplier",
-          required: true,
-        },
-        prixTVAC: {
-          type: Number,
-          required: true,
-        },
-        amountHT: [
-          {
-            "march (81)": {
-              type: Number,
-              required: true,
-            },
-            "charges (82)": {
-              type: Number,
-              required: true,
-            },
-            "invest (83)": {
-              type: Number,
-              required: true,
-            },
-          },
-        ],
-        "deductibleTVA (59)": {
-          type: Number,
-          required: true,
-        },
-        totalTVAC: {
-          type: Number,
-          required: true,
-        },
-        totalMarch: {
-          type: Number,
-          required: true,
-        },
-        totalCharges: {
-          type: Number,
-          required: true,
-        },
-        totalInvest: {
-          type: Number,
-          required: true,
-        },
-        totalDeductibleTVA: {
-          type: Number,
-          required: true,
-        },
+  },
+  data: [
+    {
+      number: {
+        type: Number,
+        required: true,
       },
-    ],
+      date: {
+        type: Date,
+        required: true,
+      },
+      supplier: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "supplier",
+        required: true,
+      },
+      priceTVAC: {
+        type: Number,
+        required: true,
+      },
+      amountHT: [
+        {
+          "march (81)": {
+            type: Number,
+            required: true,
+          },
+          "charges (82)": {
+            type: Number,
+            required: true,
+          },
+          "invest (83)": {
+            type: Number,
+            required: true,
+          },
+        },
+      ],
+      "deductibleTVA (59)": {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  totalTVAC: {
+    type: Number,
+    required: true,
+  },
+  totalMarch: {
+    type: Number,
+    required: true,
+  },
+  totalCharges: {
+    type: Number,
+    required: true,
+  },
+  totalInvest: {
+    type: Number,
+    required: true,
+  },
+  totalDeductibleTVA: {
+    type: Number,
+    required: true,
   },
 });
 
