96123a73c095185d773ab9ef5474f0429840a204
tim <timdesmet00@gmail.com>
2024-12-30 07:50:10 +0100

    model processInvoiceIn

---
+++ Models/processInvoiceIn.model.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const ProcessInvoiceInSchema = new mongoose.Schema({
+  period: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^(0[1-9]|1[0-2])-(\d{4})$/.test(v); // Valide le format MM-YYYY
+      },
+      message: (props) =>
+        `${props.value} is not a valid period format! Use MM-YYYY.`,
+    },
+    data: [
+      {
+        number: {
+          type: Number,
+          required: true,
+        },
+        date: {
+          type: Date,
+          required: true,
+        },
+        supplier: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "supplier",
+          required: true,
+        },
+        totalTVAC: {
+          type: Number,
+          required: true,
+        },
+        amountHT: [
+          {
+            "march (81)": {
+              type: Number,
+              required: true,
+            },
+            "charges (82)": {
+              type: Number,
+              required: true,
+            },
+            "invest (83)": {
+              type: Number,
+              required: true,
+            },
+          },
+        ],
+        "deductibleTVA (59)": {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+  },
+});
