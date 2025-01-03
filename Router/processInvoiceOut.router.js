e6e2f73a60d162abd45a8c15aa58892998f7ae4f
tim <timdesmet00@gmail.com>
2025-01-03 09:17:40 +0100

    processInvoiceOut router

---
+++ Router/processInvoiceOut.router.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.router();
+const ^rocessInvoiceOutController = require("../Controllers/processInvoiceOut.controller");
+
+router.post("/add", processInvoiceOutController.createProcessInvoiceOut);
+router.get("/getall", processInvoiceOutController.getAllProcessInvoiceOut);
+router.get("/getone/:id", processInvoiceOutController.getProcessInvoiceOutById);
+router.patch("/update/:id", processInvoiceOutController.updateProcessInvoiceOut);
+router.patch(
+  "/close/:id",
+  processInvoiceOutController.updateAndCloseProcessInvoiceOut
+);
+router.delete("/delete/:id", processInvoiceOutController.deleteProcessInvoiceOut);
+
+module.exports = router;
