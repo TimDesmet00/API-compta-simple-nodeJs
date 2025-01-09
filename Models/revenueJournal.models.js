1db783e7c3716b533c62391c69398dbe8a0b3109
tim <timdesmet00@gmail.com>
2025-01-09 17:14:08 +0100

    premier jet fini du model revenueJournal

--- Models/revenueJournal.models.js
+++ Models/revenueJournal.models.js
@@ -49,4 +49,76 @@ const RevenueJournalSchema = new mongoose.Schema({
     type: Number,
     required: false,
   },
+  totalTvac: [
+    {
+      "0%": {
+        type: Number,
+        required: false,
+      },
+      "6%": {
+        type: Number,
+        required: false,
+      },
+      "12%": {
+        type: Number,
+        required: false,
+      },
+      "21%": {
+        type: Number,
+        required: false,
+      },
+      totalCash: {
+        type: Number,
+        required: false,
+      },
+    },
+  ],
+  totalHTVA: [
+    {
+      "0% 00": {
+        type: Number,
+        required: false,
+      },
+      "6% 01": {
+        type: Number,
+        required: false,
+      },
+      "12% 02": {
+        type: Number,
+        required: false,
+      },
+      "21% 03": {
+        type: Number,
+        required: false,
+      },
+      totalCash: {
+        type: Number,
+        required: false,
+      },
+    },
+  ],
+  totalTVA: [
+    {
+      "0%": {
+        type: Number,
+        required: false,
+      },
+      "6%": {
+        type: Number,
+        required: false,
+      },
+      "12%": {
+        type: Number,
+        required: false,
+      },
+      "21%": {
+        type: Number,
+        required: false,
+      },
+      "totalTVA 54": {
+        type: Number,
+        required: false,
+      },
+    },
+  ],
 });
